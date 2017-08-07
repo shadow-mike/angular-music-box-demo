1bbd12354a6c7383bcdc30bc68cb3dc154d5bc81
shadow512122313 <512122313@qq.com>
2017-08-08 00:06:28 +0800

    update component: player

--- src/app/shared/player/player.component.ts
+++ src/app/shared/player/player.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { Album } from '../../shared/class/album';
 
 @Component({
   selector: 'm-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlayerComponent implements OnInit {
 
+
+  mcl: number = 0; // music current length
   @Input() album: Album = new Album();
-  constructor() { }
+  @ViewChild('audio') audio: ElementRef;
+  @ViewChild('audioBtn') audioBtn: ElementRef;
+
+  constructor(
+    public renderer: Renderer2
+  ) { }
 
   ngOnInit() {
-    this.album.music = 'http://m10.music.126.net/20170807151441/ac9d9bcf21011543b31ad34d7b4e1963/ymusic/fd75/c069/b7d0/b24b661d90b6d82ff42730c355c91c6b.mp3';
+    this.album.music = 'http://m10.music.126.net/20170807222129/88261227856ef422f9f4ebfa77db90ac/ymusic/7d5f/aaeb/36d1/3921301e60fddf07aa85f016a00241f3.mp3';
+    this.renderer.listen(this.audioBtn.nativeElement, 'click', (event) => {
+      console.log(event)
+      // 判断状态，定时器修改mcl
+    })
   }
+
+  // ngDoCheck() {
+  //   console.log(this.mcl)
+  //   this.mcl = (this.audio.nativeElement.currentTime / this.audio.nativeElement.duration) * 100;
+  // }
 
 }
